cbe3a04e6a232c345a340d73a5b74312a6e8dc0a
Adriana-Drika <nmdrika@gmail.com>
2023-01-26 18:09:09 -0300

    criação da parte detalhes do filme

--- projetofilmes/src/pages/Home/index.js
+++ projetofilmes/src/pages/Home/index.js
@@ -1,15 +1,18 @@
-import {useEffect, useState } from 'react'
-import api from '../../services/api'
+import {useEffect, useState } from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 ////URL da API: /movie/now_playing?api_key=9fbddd26497885ea4147698f1e9eeeb9&language=pt-BR
 
 function Home(){
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true)
 
   useEffect(()=>{
 
     async function loadFilmes(){
-      const response = await api.get('/movie/now_playing', {
+      const response = await api.get('movie/now_playing', {
         params:{
           api_key:'9fbddd26497885ea4147698f1e9eeeb9',
           language: 'pt-BR',
@@ -17,7 +20,9 @@ function Home(){
         }
       })
 
-      console.log(response.data.results);
+      // console.log(response.data.results.slice(0,10));
+      setFilmes(response.data.results.slice(0,10))
+      setLoading(false);
 
     }
 
@@ -25,9 +30,29 @@ function Home(){
 
   }, [])
 
+  if(loading){
+    return(
+      <div classNAme='loading'>
+        <h2>Carregando filmes ...</h2>
+      </div>
+    )
+  }
+
+
+
   return(
-    <div>
-       <h1>Bem vindo a Home</h1>
+    <div className='container'>
+      <div className='lista-filmes'>
+         {filmes.map((filme) => {
+           return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+           )
+         })}
+      </div>
     </div>
   )
 }
