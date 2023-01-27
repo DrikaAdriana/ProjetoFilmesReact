1761a4b514dab8a0b207983e39e6b989928d094a
Adriana-Drika <nmdrika@gmail.com>
2023-01-27 14:12:43 -0300

    Projeto filmes finalizado

--- projetofilmes/src/pages/Filme/index.js
+++ projetofilmes/src/pages/Filme/index.js
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import './filme-info.css'; 
 import api from '../../services/api'
+import { toast }from 'react-toastify'
+
 
 function Filme(){
   const {id} = useParams();
+  const navigate = useNavigate();
+
   const [filme, setFilme] = useState({});
   const [loading, setLoading] = useState(true);
 
@@ -22,6 +26,8 @@ function Filme(){
       })
       .catch(()=>{
         console.log('FILME NÃO ENCONTRADO!!');
+        navigate('/', { replace: true});
+        return;
       })
     }
 
@@ -30,7 +36,27 @@ function Filme(){
     return() => {
       console.log('COMPONENTE FOI DESMONTADO')
     }
-  }, [])
+  }, [navigate, id])
+
+
+  function salvarFilme(){
+    const minhaLista = localStorage.getItem('@primeflix');
+
+    let filmesSalvos = JSON.parse(minhaLista) || [];
+
+    const hasFilme = filmesSalvos.some( (filmesSalvo) => filmesSalvo.id === filme.id)
+
+    if(hasFilme){
+      toast.warn('Este filme já está na sua lista');
+      return;
+    }
+
+    filmesSalvos.push(filme);
+    localStorage.setItem('@primeflix', JSON.stringify(filmesSalvos));
+    toast.success('Filme salvo com sucesso');
+  }  
+
+
 
   if(loading){
     return(
@@ -50,9 +76,9 @@ function Filme(){
       <strong>Avaliação: {filme.vote_average} / 10</strong>
 
       <div className='area-buttons'>
-        <button>Salvar</button>
+        <button onClick={salvarFilme}>Salvar</button>
         <button>
-          <a href='#'>
+          <a target='blank' rel='external'href= {`https://youtube.com/results?search_query=${filme.title} Trailer`}>
             Trailer
           </a>
         </button>
